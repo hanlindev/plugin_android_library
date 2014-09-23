eadea6b411142bf7395bd112bec55ea4111385cf
Zhao Yue <endlessfrank@qq.com>
2014-09-23 15:59:21 +0800

    Merge

--- PluginAndroidLibrary/plugin_android_library/src/main/java/com/sana/android/plugin/hardware/BluetoothDevice.java
+++ PluginAndroidLibrary/plugin_android_library/src/main/java/com/sana/android/plugin/hardware/BluetoothDevice.java
@@ -1,14 +1,14 @@
 package com.sana.android.plugin.hardware;
 
 import android.content.ContentResolver;
-import android.media.MediaRecorder;
 
 import com.sana.android.plugin.application.CommManager;
 import com.sana.android.plugin.data.BinaryDataWithPollingEvent;
 import com.sana.android.plugin.data.DataWithEvent;
 
 import java.io.InputStream;
-
+import java.io.FileInputStream;
+import java.io.File;
 /**
  * Created by hanlin on 9/14/14.
  */
@@ -18,9 +18,10 @@ public class BluetoothDevice extends AudioRecordDevice implements GeneralDevice 
     @Override
     //prepare is to store location of recorded audio?
     public DataWithEvent prepare() {
-        //CommManager cm = CommManager.getInstance();
-        //InputStream is =
-        //BinaryDataWithPollingEvent pollingData = new BinaryDataWithPollingEvent(Feature.BLUETOOTH, CommManager.getInstance().getMimeType(), cm.getUri(), this, int uri_size);
+        CommManager cm = CommManager.getInstance();
+        File initialFile = new File(super.mFileName);
+        InputStream is = new FileInputStream(initialFile);
+        BinaryDataWithPollingEvent pollingData = new BinaryDataWithPollingEvent(Feature.BLUETOOTH, CommManager.getInstance().getMimeType(), cm.getUri(), this, int uri_size);
         return null;
     }
 
