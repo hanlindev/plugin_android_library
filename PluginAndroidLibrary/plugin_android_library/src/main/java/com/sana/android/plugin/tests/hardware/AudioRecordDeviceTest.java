263284f72c85f0f71413bf00ccbd167ccbc2a62c
yanfei199311@gmail.com <yanfei199311@gmail.com>
2014-10-07 15:28:23 +0800

    some tests for audio recording

---
+++ PluginAndroidLibrary/plugin_android_library/src/main/java/com/sana/android/plugin/tests/hardware/AudioRecordDeviceTest.java
@@ -0,0 +1,56 @@
+package com.sana.android.plugin.tests.hardware;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.test.InstrumentationTestCase;
+import android.util.Log;
+
+import com.sana.android.plugin.hardware.AudioRecordDevice;
+
+/**
+ * Created by Mia on 7/10/14.
+ */
+public class AudioRecordDeviceTest extends InstrumentationTestCase  {
+    private AudioRecordDevice audioRecord = new AudioRecordDevice();
+    private static final String TAG = "AudioRecordDeviceTest";
+
+
+    public void testPauseRecorder(){
+        audioRecord.reset();
+        audioRecord.prepare();
+        audioRecord.begin();
+        audioRecord.pauseRecorder();
+        assertEquals(audioRecord.getmRecorder(),null);
+    }
+
+    public void testPausePlayer(){
+        audioRecord.reset();
+        audioRecord.prepare();
+        audioRecord.begin();
+        audioRecord.pausePlayer();
+        assertEquals(audioRecord.getmPlayer(),null);
+    }
+
+    public void testBegin(){
+
+    }
+
+    public void testStop(){
+
+    }
+
+    public void testReset(){
+
+    }
+
+    public void testStartPlaying(){
+
+    }
+
+    public void testStopPlaying(){
+
+    }
+}
