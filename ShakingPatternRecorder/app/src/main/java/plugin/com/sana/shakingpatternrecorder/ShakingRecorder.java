9cf805ab1f5feb88b80bc81724a835cdc6768604
Han Lin <hanlin.dev@gmail.com>
2014-10-21 20:59:53 +0800

    Add pretty button to shaky app.

--- ShakingPatternRecorder/app/src/main/java/plugin/com/sana/shakingpatternrecorder/ShakingRecorder.java
+++ ShakingPatternRecorder/app/src/main/java/plugin/com/sana/shakingpatternrecorder/ShakingRecorder.java
@@ -1,25 +1,84 @@
 package plugin.com.sana.shakingpatternrecorder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.sana.android.plugin.application.CaptureManager;
 import com.sana.android.plugin.application.CommManager;
+import com.sana.android.plugin.communication.MimeType;
+import com.sana.android.plugin.data.event.AccelerometerDataEvent;
+import com.sana.android.plugin.data.listener.TimedListener;
+import com.sana.android.plugin.hardware.CaptureSetting;
+import com.sana.android.plugin.hardware.Feature;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class ShakingRecorder extends ActionBarActivity {
+    private class AccelerometerEventListener extends TimedListener {
+        public AccelerometerEventListener(Object sender) {
+            super(sender, 1, TimeUnit.SECONDS);
+        }
+
+        @Override
+        public synchronized void processData(Object sender, Object[] data) {
+            if (data.length > 0) {
+                Log.d(AccelerometerEventListener.class.getName(), "Received data from event hahaha");
+                Message msg = handler.obtainMessage();
+                msg.obj = data[data.length - 1];
+                handler.sendMessage(msg);
+            }
+        }
+    }
+
+    private CaptureManager captureManager;
+    private AccelerometerEventListener listener;
+    private boolean isRecording;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            TextView textX = (TextView)findViewById(R.id.textViewX);
+            TextView textY = (TextView)findViewById(R.id.textViewY);
+            TextView textZ = (TextView)findViewById(R.id.textViewZ);
+
+            AccelerometerDataEvent.AccelerometerData data = (AccelerometerDataEvent.AccelerometerData) msg.obj;
+            double x = data.getX();
+            double y = data.getY();
+            double z = data.getZ();
+            textX.setText(x + ", ");
+            textY.setText(y+", ");
+            textZ.setText(z+"");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shaking_recorder);
 
+
         // receive launch intent from sana
         Intent intent = getIntent();
         CommManager cm = CommManager.getInstance();
         cm.respondToIntent(intent);
+
+        // Prepare CaptureManager
+        CaptureSetting newSetting = CaptureSetting.defaultSetting(Feature.ACCELEROMETER, MimeType.TEXT_PLAIN)
+                .setSensorManager((SensorManager) getSystemService(Context.SENSOR_SERVICE));
+        captureManager = new CaptureManager(Feature.ACCELEROMETER, MimeType.TEXT_PLAIN, getContentResolver(), newSetting);
+        listener = new AccelerometerEventListener(captureManager);
+        captureManager.addListener(listener);
+        captureManager.prepare();
     }
 
 
